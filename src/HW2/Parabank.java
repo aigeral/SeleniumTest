4d790a661311ad315ae9a37ab9fdaf40e8f7dbf1
aigeral <kairbekova.aig@gmail.com>
2023-02-28 19:47:06 +0300

    Parabank

---
+++ src/HW2/Parabank.java
@@ -0,0 +1,10 @@
+package HW2;
+
+public class Parabank {
+     System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+
+    WebDriver driver=new ChromeDriver();
+    //open up fb.com
+        driver.get("https://www.facebook.com/");
+
+}
