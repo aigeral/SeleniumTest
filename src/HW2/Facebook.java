0bac81da7a76933d7fb1966aebee860dc19d161f
aigeral <kairbekova.aig@gmail.com>
2023-02-28 22:04:30 +0300

    HW2

---
+++ src/HW2/Facebook.java
@@ -0,0 +1,46 @@
+package HW2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.facebook.com/");
+
+        driver.manage().window().maximize();
+        //navigate to fb.com
+        //click on create new account
+        //fill up all the textboxes
+        //click on sign up button
+        //close the pop up
+        //close the browser
+        //TIP for HW2:
+        //just fill in text boxes or button, no need to deal with other webElements
+
+
+        driver.findElement(By.linkText("Create new account")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.name("firstname")).sendKeys("Aigerim");
+        driver.findElement(By.name("lastname")).sendKeys("Kairbekova");
+        driver.findElement(By.name("reg_email_")).sendKeys("676r34634750834");
+        driver.findElement(By.name("reg_email_confirmation_")).sendKeys("676r34634750834");
+        driver.findElement(By.name("reg_passwd_")).sendKeys("Aa3456789");
+        driver.findElement(By.name("birthday_day")).sendKeys("1");
+        driver.findElement(By.name("birthday_month")).sendKeys("Jun");
+        driver.findElement(By.name("birthday_year")).sendKeys("1992");
+        driver.findElement(By.name("sex")).click();
+
+        Thread.sleep(3000);
+        driver.findElement(By.name("websubmit")).click();
+        Thread.sleep(2000);
+        driver.close();
+        Thread.sleep(2000);
+        driver.quit();
+
+
+    }
+
+}
--- src/HW2/Parabank.java
+++ src/HW2/Parabank.java
@@ -1,10 +1,46 @@
 package HW2;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
+
 public class Parabank {
-     System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get ("https://parabank.parasoft.com/parabank/register.htm;jsessionid=B8DD31FF0AFEDBDF4454B27BBC5C05B0%22");
+        driver.manage().window().maximize();
+        //fill out the form
+        //click on register
+        //close the browser
 
-    WebDriver driver=new ChromeDriver();
-    //open up fb.com
-        driver.get("https://www.facebook.com/");
+        driver.findElement(By.id("customer.firstName")).sendKeys("Aigerim");
+        driver.findElement(By.id("customer.lastName")).sendKeys("Kairbekova");
+        driver.findElement(By.id("customer.address.street")).sendKeys("Fintas,block2");
+        driver.findElement(By.id("customer.address.city")).sendKeys("Kuwait");
+        driver.findElement(By.id("customer.address.state")).sendKeys("Kuwait");
+        driver.findElement(By.id("customer.address.zipCode")).sendKeys("63000");
+        driver.findElement(By.id("customer.phoneNumber")).sendKeys("63694995");
+        driver.findElement(By.id("customer.ssn")).sendKeys("7644800885679");
 
+        driver.findElement(By.id("customer.username")).sendKeys("aigeral");
+        driver.findElement(By.id("customer.password")).sendKeys("1234567");
+        driver.findElement(By.id("repeatedPassword")).sendKeys("1234567");
+
+        Thread.sleep(3000);
+
+        driver.findElement(By.className("button")).click();
+
+        Thread.sleep(3000);
+
+        driver.quit();
+
+
+
+
+    }
 }
